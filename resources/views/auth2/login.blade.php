6d6d316f9e40f8e6da82cfcd57385c7240cf3c48
dev1webbixray <dev1.webbixray@gmail.com>
2023-10-12 17:02:31 +0630

    dashboard
    fvcv

--- resources/views/auth2/login.blade.php
+++ resources/views/auth2/login.blade.php
@@ -4,8 +4,6 @@
             <x-authentication-card-logo />
         </x-slot>
 
-        <x-validation-errors class="mb-4" />
-
         @if (session('status'))
             <div class="mb-4 font-medium text-sm text-green-600">
                 {{ session('status') }}
