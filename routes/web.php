7b99a7aef1c3c9846c6e359fcce292e3e2d595be
qqewaiyan <waiyanphyomg2002@gmail.com>
2023-10-12 15:14:04 +0630

    login commit

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::middleware([
     'auth:sanctum',
@@ -25,4 +23,9 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+Route::middleware(["auth"])->group(function(){
+    Route::get('/', function () {
+        return view("auth.login");
+    });
 });
